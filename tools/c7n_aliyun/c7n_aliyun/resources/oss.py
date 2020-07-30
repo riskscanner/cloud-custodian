3132623b470fa9b94836440f372c7982e1b0bd0c
maguohao2018 <guohao.ma@fit2cloud.com>
2020-07-30 17:06:55 +0800

    feat(c7n_aliyun): 修改阿里云oss

--- tools/c7n_aliyun/c7n_aliyun/resources/oss.py
+++ tools/c7n_aliyun/c7n_aliyun/resources/oss.py
@@ -11,14 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
 
-from aliyunsdkecs.request.v20140526.DeleteDiskRequest import DeleteDiskRequest
+import oss2
 from c7n_aliyun.actions import MethodAction
+from c7n_aliyun.client import REGION_ENDPOINT
 from c7n_aliyun.provider import resources
 from c7n_aliyun.query import QueryResourceManager, TypeInfo
 
 from c7n.utils import type_schema
 
+service = 'oss'
+accessKeyId = os.getenv('ALIYUN_ACCESSKEYID')
+accessSecret = os.getenv('ALIYUN_ACCESSSECRET')
+regionId = os.getenv('ALIYUN_DEFAULT_REGION')
+auth = oss2.Auth(accessKeyId, accessSecret)
 
 @resources.register('oss')
 class Oss(QueryResourceManager):
@@ -26,21 +33,102 @@ class Oss(QueryResourceManager):
     class resource_type(TypeInfo):
         service = 'oss'
         enum_spec = (None, 'buckets', None)
-        id = 'DiskId'
+        id = 'id'
 
     def get_requst(self):
         pass
 
 
-@Oss.action_registry.register('delete')
-class OssDelete(MethodAction):
+@Oss.action_registry.register('create_bucket')
+class OssCreateBucket(MethodAction):
 
-    schema = type_schema('delete')
-    method_spec = {'op': 'delete'}
-    attr_filter = ('Status', ('Available', ))
+    schema = type_schema('create_bucket')
+    method_spec = {'op': 'create_bucket'}
 
-    def get_requst(self, disk):
-        request = DeleteDiskRequest()
-        request.set_DiskId(disk['DiskId'])
-        request.set_accept_format('json')
-        return request
+    def get_requst(self, bucket):
+        try:
+            bucket = oss2.Bucket(auth, REGION_ENDPOINT[regionId], bucket['bucketname'])
+            bucketConfig = oss2.models.BucketCreateConfig(oss2.BUCKET_STORAGE_CLASS_STANDARD,
+                                                          oss2.BUCKET_DATA_REDUNDANCY_TYPE_ZRS)
+            resp = bucket.create_bucket(oss2.BUCKET_ACL_PRIVATE, bucketConfig)
+            if resp.status < 300:
+                # 操作成功
+                print('requestId:', resp.requestId)
+                # 处理操作成功后业务逻辑
+                return resp
+            else:
+                # 操作失败，获取详细异常信息
+                return resp.errorMessage
+        except Exception as e:
+            import traceback
+            # 发生异常，打印异常堆栈
+            return traceback.format_exc()
+
+@Oss.action_registry.register('put_object')
+class OssPutObject(MethodAction):
+    schema = type_schema('put_object')
+    method_spec = {'op': 'put_object'}
+
+    def get_requst(self, bucket):
+        try:
+            headers = dict()
+            headers["x-oss-storage-class"] = "Standard"
+            headers["x-oss-object-acl"] = oss2.OBJECT_ACL_PRIVATE
+            bucket = oss2.Bucket(auth, REGION_ENDPOINT[regionId], bucket['bucketname'])
+            resp = bucket.put_object(bucket['objectname'], bucket['obj'], headers)
+            if resp.status < 300:
+                # 操作成功
+                print('requestId:', resp.requestId)
+                # 处理操作成功后业务逻辑
+                return resp
+            else:
+                # 操作失败，获取详细异常信息
+                return resp.errorMessage
+        except Exception as e:
+            import traceback
+            # 发生异常，打印异常堆栈
+            return traceback.format_exc()
+
+@Oss.action_registry.register('get_object_to_file')
+class OssGetObject(MethodAction):
+    schema = type_schema('get_object_to_file')
+    method_spec = {'op': 'get_object_to_file'}
+
+    def get_requst(self, bucket):
+        try:
+            bucket = oss2.Bucket(auth, REGION_ENDPOINT[regionId], bucket['bucketname'])
+            resp = bucket.get_object_to_file(bucket['objectname'], bucket['localFile'])
+            if resp.status < 300:
+                # 操作成功
+                print('requestId:', resp.requestId)
+                # 处理操作成功后业务逻辑
+                return resp
+            else:
+                # 操作失败，获取详细异常信息
+                return resp.errorMessage
+        except Exception as e:
+            import traceback
+            # 发生异常，打印异常堆栈
+            return traceback.format_exc()
+
+@Oss.action_registry.register('delete_object')
+class OssDeleteObject(MethodAction):
+    schema = type_schema('delete_object')
+    method_spec = {'op': 'delete_object'}
+
+    def get_requst(self, bucket):
+        try:
+            bucket = oss2.Bucket(auth, REGION_ENDPOINT[regionId], bucket['bucketname'])
+            resp = bucket.delete_object(bucket['objectname'])
+            if resp.status < 300:
+                # 操作成功
+                print('requestId:', resp.requestId)
+                # 处理操作成功后业务逻辑
+                return resp
+            else:
+                # 操作失败，获取详细异常信息
+                return resp.errorMessage
+        except Exception as e:
+            import traceback
+            # 发生异常，打印异常堆栈
+            return traceback.format_exc()
