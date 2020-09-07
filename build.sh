5f781327c83fd7a7f0e153eb82ebef4bc0d977f4
maguohao2018 <guohao.ma@fit2cloud.com>
2020-09-07 21:21:15 +0800

    feat(Dockerfile): 修改镜像标签

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 echo "构建custodian镜像 ..."
-docker build -t registry.fit2cloud.com/fit2cloud2/custodian:mgh .
-docker push registry.fit2cloud.com/fit2cloud2/custodian:mgh
+docker build -t registry.fit2cloud.com/fit2cloud2/custodian:base .
+docker push registry.fit2cloud.com/fit2cloud2/custodian:base
